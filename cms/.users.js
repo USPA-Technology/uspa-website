d4b570e0bdb8ce1bcfb7aa4fdd0c9796840d0d07
iamhung <phamdongochung@gmail.com>
2020-07-03 14:49:15 +0700

    enduro admin add admin admin@123

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '317c4c169252a6f606d0dd8bcaefc9bc',
+			hash: '41e3d2d3595aa07930fe684b66969e49fc3fcf41b552532628abf1a1b801dc55',
+			user_created_timestamp: 1593762146789
+		}
+	]
+})
