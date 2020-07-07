9473156c085d357c65d289c69b5ca070dc5fe9cb
iamhung <phamdongochung@gmail.com>
2020-07-07 16:21:39 +0700

    test page with cms

---
+++ cms/features.js
@@ -0,0 +1,7 @@
+({
+	features_iteams: {
+		header: 'features 1',
+		tagline: 'ABC',
+		call_to_action: 'Scroll down to see what\'s up'
+	}
+})
---
+++ cms/pricing.js
@@ -0,0 +1,19 @@
+({
+  "pricing_iteams": {
+    "free": {
+      "header": "Free",
+      "tagline": "ABC",
+      "call_to_action": "Scroll down to see what up"
+    },
+    "pro": {
+      "header": "Professional",
+      "tagline": "ABC",
+      "call_to_action": "Scroll down to see what up"
+    },
+    "enterprise": {
+      "header": "Enterprise",
+      "tagline": "ABC",
+      "call_to_action": "Scroll down to see what up"
+    }
+  }
+})
